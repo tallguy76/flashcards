505f462d8609cc72c27f504c87519f8231eb9d4e
Jeffrey Willson <peacetoawarship@gmail.com>
2013-11-14 16:04:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flashcards::Application.config.secret_key_base = '6a434500fba2463d2a3db6a4a1985b3d52f889e38e3d3f2e60c396455d76dc2735ed8bddde625ff242bf1a253f4a07ce88f97465d7d65d59c279990e7dea0449'
---
+++ public/assets/js/app.js
@@ -0,0 +1,193 @@
+"use strict";
+var _ = window._;
+var $ = window.$;
+var vocab;
+var run = [{"trad":"鐵","simp":"铁","pinyin":"tiě","gcr":"tiet","definition":"iron"}];
+var cur = 0;
+var lastWrong = false;
+var incorrect = [];
+var charFormat = {primary:"trad",secondary:"simp"};
+var hidden = {entry: false, hint: true, answer: true, "alt-pinyin": true, "alt-char": true};
+var disabled = {"show": false, previous: true, show_hint: false, correct: true, incorrect: true};
+
+var showAns = function () {
+  hidden = {entry: false, hint: false, answer: false, "alt-pinyin": false, "alt-char": false};
+  $(".hint").removeClass("invisible");
+  $(".answer").removeClass("invisible");
+  disabled = {"show": true, previous: true, show_hint: true, correct: false, incorrect: false};
+  $("#show").prop( "disabled", true );
+  $("#show_hint").prop( "disabled", true );
+  $("#previous").prop( "disabled", true );
+  $("#correct").prop( "disabled", false );
+  $("#incorrect").prop( "disabled", false );
+};
+var hideAns = function () {
+  hidden = {entry: false, hint: true, answer: true, "alt-pinyin": true, "alt-char": true};
+  $(".hint").addClass("invisible");
+  $(".answer").addClass("invisible");
+  disabled = {"show": false, show_hint: false,previous: false,  correct: true, incorrect: true};
+  $("#show").prop( "disabled", false );
+  $("#show_hint").prop( "disabled", false );
+  $("#previous").prop( "disabled", false );
+  $("#correct").prop( "disabled", true );
+  $("#incorrect").prop( "disabled", true );
+};
+var showNext = function () {
+  if(++cur < run.length){
+    hideAns();
+    displayItem(run[cur]);
+  } else {
+    $("#summary").html(run.length + " items, " + incorrect.length + " incorrect");
+    $("#restart").removeClass("latent");
+  }
+};
+var showLast = function () {
+  if(cur > 0) cur--;
+  displayItem(run[cur]);
+  showAns();
+  if(lastWrong) incorrect.pop();
+};
+
+var enableButtons = function () {
+  $("#container").delegate("button", "click", function(e){
+    buttonHandlers[this.id]();
+  });
+};
+
+var altDisplay;
+var listing = {
+  stimulus: "characters",
+  response: "pinyin"
+};
+var switchDisplay = function () {
+  var oldDisplay = $("#display").html();
+  $("#display").html(altDisplay);
+  altDisplay = oldDisplay;
+  listing = {
+    stimulus: listing.response,
+    response: listing.stimulus
+  };
+  $("#toggle_display").html("Prompt with " + listing.response);
+  displayItem(run[cur]);
+  showAns();
+};
+
+var buttonHandlers = {
+  show: showAns,
+  show_hint: function () {
+    hidden.hint = false;
+    $(".hint").removeClass("invisible");
+    disabled.show_hint = true;
+    $("#show_hint").prop( "disabled", true );
+  },
+  correct: function () {
+    lastWrong = false;
+    showNext();
+  },
+  incorrect: function () {
+    incorrect.push(run[cur]);
+    lastWrong = true;
+    showNext();
+  },
+  previous: showLast,
+  toggle_format: function () {
+    charFormat = {primary: charFormat.secondary, secondary: charFormat.primary};
+    var secondaryName =
+      charFormat.secondary === "trad" ?
+      "traditional" : "simplified";
+    $("#toggle_format").html(
+      "Use " + secondaryName + " characters"
+    );
+    displayItem(run[cur]);
+  },
+  review_all: function () {
+    $("#summary").html("<p>Last run: " + run.length + " items, " + incorrect.length + " incorrect</p>" +
+      "<p>Reviewing the entire list<p>"
+    );
+    run = _.shuffle(vocab);
+    begin_run();
+  },
+  review_incorrect: function () {
+    $("#summary").html("<p>Last run: " + run.length + " items, " + incorrect.length + " incorrect</p>" +
+      "<p>Reviewing those missed<p>"
+    );
+    run = _.shuffle(incorrect);
+    begin_run();
+  },
+  toggle_display: switchDisplay
+};
+
+var respond = function(name) {
+  if(!disabled[name]) {
+    $("#" + name).trigger("click");
+  }
+};
+var enableKeyShortcuts = function () {
+    $(document).keydown(function (e) {
+    switch(e.which){
+    case 32:
+      e.preventDefault();
+      respond("show");
+    break;
+    case 66:
+    case 98:
+      e.preventDefault();
+      respond("show_hint");
+    break;
+    case 8:
+      e.preventDefault();
+      respond("previous");
+    break;
+    case 86:
+    case 118:
+      e.preventDefault();
+      respond("correct");
+    break;
+    case 78:
+      e.preventDefault();
+      respond("incorrect");
+    break;
+    }
+  });
+};
+
+var displayItem = function (obj) {
+  $("#entry").children("span").html(obj[charFormat.primary]);
+  $("#hint").children("span").html(obj.definition);
+  $("#pinyin").children("span").html(obj.pinyin);
+  $("#alt-pinyin").children("span").html(obj.gcr);
+  $("#alt-char").children("span").html(obj[charFormat.secondary]);
+};
+
+var begin_run = function () {
+  cur = 0;
+  incorrect = [];
+  hideAns();
+  $("#previous").prop( "disabled", true );
+  disabled.previous = true;
+  $("#restart").addClass("latent");
+  displayItem(run[cur]);
+}
+
+
+var appInit = function () {
+  run = _.shuffle(vocab);
+  begin_run();
+
+  enableButtons();
+  enableKeyShortcuts();
+  altDisplay = $("#prompt-pinyin").html();
+};
+
+$(function () {
+  $.ajax({
+    url: "assets/js/imb_v.json",
+    success: function (data) {
+      vocab = data;
+      appInit();
+    },
+    dataType: "json"
+  });
+});
+
+
